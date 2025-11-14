b4dc502dc1610f749796139a55bcddc110f8348a
Sibgatullah <sibgatullah881@gmail.com>
2025-11-14 19:15:11 +0600

    assert testing method

--- 03-Method for testing code/__name__ and __main__.py
+++ 03-Method for testing code/__name__ and __main__.py
@@ -12,3 +12,16 @@ if __name__ == "__main__":
         print("test passed")
     else:
         print("test failed!", "received:",result, "expected:",expected)
+        
+'''
+Every python file has a spacial built-in variable called __name__.
+If the file is being run directly then python sets __name__ = __main__.
+If the file is imported as a module int anther file the python sets __name__ = "--modulename--"
+
+
+This prevents accidental code execution when importing.
+lets the file be both a script and a module at the same time .
+
+This says: Only run this code when the file is executed directly
+
+'''
---
+++ 03-Method for testing code/assert.py
@@ -0,0 +1,11 @@
+'''
+Assert in Python is a debugging tool that is used to make sure something is true,
+If the condition is true then nothing happens.
+If the condition is false then Python raises an AssertionError and stops the program.
+
+use assert for :
+    To catch bugs early 
+    To validate assumptions during development
+    Helps to test logic without writing extra code 
+    
+'''
